dcfaf65f9e2a51906d214829ae7b9e823c092f84
Isimael <mrisimaelzabadia@gmail.com>
2024-09-30 22:36:35 +0200

    Development changes to web app

---
+++ Pages/Dashboard.cshtml.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GiftOfTheGiversFoundation.Pages;
+
+public class DashboardModel : PageModel
+{
+    private readonly ILogger<DashboardModel> _logger;
+
+    public DashboardModel(ILogger<DashboardModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}

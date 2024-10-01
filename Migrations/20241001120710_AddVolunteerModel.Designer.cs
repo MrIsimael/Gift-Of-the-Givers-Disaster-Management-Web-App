5e6bdad5f77d6e76f60e53c9ba6303b6a8ad449d
Isimael <mrisimaelzabadia@gmail.com>
2024-10-01 15:22:20 +0200

    Volunteer working succesfully

--- Migrations/20241001120710_AddVolunteerModel.Designer.cs
+++ Migrations/20241001120710_AddVolunteerModel.Designer.cs
@@ -4,6 +4,7 @@ using GiftOfTheGiversFoundation.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GiftOfTheGiversFoundation.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241001120710_AddVolunteerModel")]
+    partial class AddVolunteerModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

b91b9f3b2ae595b21892bbc9fd06f375238ecd4c
Isimael <mrisimaelzabadia@gmail.com>
2024-10-01 17:03:52 +0200

    Fixed SignUp Padding

--- obj/Debug/net8.0/GiftOfTheGiversFoundation.AssemblyInfo.cs
+++ obj/Debug/net8.0/GiftOfTheGiversFoundation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GiftOfTheGiversFoundation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e127eebe1bf02d297e5141632b8d192108c20c64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93cd40700110cf463378ceb55fc0c5c686c2c7d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GiftOfTheGiversFoundation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GiftOfTheGiversFoundation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
